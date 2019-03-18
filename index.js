f76f050ac9f2dfa498ca9ec31c82a90267ecade0
Madhavan Sundararaj <madhavansundararaj@gmail.com>
2019-03-19 10:33:38 +1100

    Final commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var bookList = [];
+var counter=0;
+var addedBooks = [];
 function getJsonObject(path, success, error) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -13,44 +16,42 @@ function getJsonObject(path, success, error) {
     xhr.send();
 }
 
-bookList = []; // book list container
-var counter=0;
 window.onload = function() {
     getJsonObject('data.json',
         function(data) {
-            bookList = data; // store the book list into bookList
+            bookList = data;
             var tbody = document.getElementById("rows");
-            var tr = "";
-            for(i in bookList) {
-                tr += "<tr>" + "<td>" + "<input type=\"checkbox\" class=\"checkBox\" name=\"book" + i + "\">" + "</td>" + 
-                                "<td>" + "<img src=\" " + bookList[i]['img'] + "\">" + "</td>" + 
-                                "<td>" + bookList[i]['title'] + "</td>" +
-                                "<td>" + bookList[i]['authors'] + "</td>" + 
-                                "<td>" + bookList[i]['year'] + "</td>" +
-                                "<td>" + bookList[i]['price'] + "</td>" + 
-                                "<td>" + bookList[i]['publisher'] + "</td>" + 
-                                "<td>" + bookList[i]['category'] + "</td>" + "</tr>";
+            var tableRow = "";
+            for(var i in bookList) {
+                tableRow += "<tr>" + "<td>" + "<input type=\"checkbox\" class=\"checkBox\" name=\"book" + i + "\">" + "</td>"
+                             + "<td>" + "<img src=\" " + bookList[i]['img'] + "\">" + "</td>"
+                             + "<td class=\"titleStyle\">" + bookList[i]['title'] + "</td>"
+                             + "<td>" + bookList[i]['authors'] + "</td>" 
+                             + "<td>" + bookList[i]['year'] + "</td>"
+                             + "<td>" + bookList[i]['price'] + "</td>" 
+                             + "<td>" + bookList[i]['publisher'] + "</td>"
+                             + "<td>" + bookList[i]['category'] + "</td>" + "</tr>";
             }
-            tbody.innerHTML = tr;
+            tbody.innerHTML = tableRow;
         },
         function(xhr) { console.error(xhr); }
 );
 }
 
-function searchFunction() {
+function titleSearch() {
     var searchInput = document.getElementById("search");
-    var searchLowerCase = searchInput.value.toLowerCase();
+    var searchUpperCase = searchInput.value.toUpperCase();
     var tbody = document.getElementById("rows");
     var tableRow = tbody.getElementsByTagName("tr");
     var tableData = "";
     var titleNotFound = 0;
-    for (i = 0; i < tableRow.length; i++) {
+    for (var i = 0; i < tableRow.length; i++) {
         tableData = tableRow[i].getElementsByTagName("td")[2];
         if (tableData) {
            var text = tableData.textContent;
-           if (text.toLowerCase().indexOf(searchLowerCase) > -1 && searchLowerCase != "") {
+           if (text.toUpperCase().indexOf(searchUpperCase) > -1 && searchUpperCase != "") {
                tableRow[i].style.backgroundColor = "gold";
-           } else if (searchLowerCase == "") {
+           } else if (searchUpperCase == "") {
                 tableRow[i].style.backgroundColor = "#FAFCFF";
            }
            else {
@@ -60,36 +61,42 @@ function searchFunction() {
         }
     }
     if (titleNotFound == 10) {
-        alert("Sorry, There is no book under this title.");
+        alert("Sorry, There is no book under " + searchInput.value + " title.");
         searchInput.value = "";
     }
 }
 
-function filterFunction() {
+function categoryFilter() {
     var filterInput = document.getElementById("filter");
-    var filterLowerCase = filterInput.value.toLowerCase();
+    var filterUpperCase = filterInput.value.toUpperCase();
     var tbody = document.getElementById("rows");
     var tableRow = tbody.getElementsByTagName("tr");
     var tableData = "";
+    var categoryNotFound = 0;
     for (i = 0; i < tableRow.length; i++) {
         tableData = tableRow[i].getElementsByTagName("td")[7];
         if (tableData) {
            var text = tableData.textContent;
-           if (text.toLowerCase().indexOf(filterLowerCase) > -1 || filterLowerCase === "category") {
+           if (text.toUpperCase().indexOf(filterUpperCase) > -1 || filterUpperCase === "category") {
                tableRow[i].style.display = "";
            } else {
+            categoryNotFound += 1
             tableRow[i].style.display = "none";
            }
         }
+    }
+    if (categoryNotFound == 10) {
+        alert("Sorry, currently there are no books under \"" +  filterInput.value + "\" category.");
     }
 }
 
 function addToCart() {
     var checkBox = document.getElementsByClassName("checkBox");
     var cartCounter = document.getElementById("counter");
-    for (i in checkBox) {
+    for (var i in checkBox) {
         if (checkBox[i].checked) {
             counter += 1;
+            addedBooks.push(bookList[i]['title']);
             checkBox[i].checked = false;
         }
     }
@@ -97,13 +104,20 @@ function addToCart() {
 }
 
 function resetCart() {
-    var reset = confirm("Click Ok to reset the cart");
+    if (addedBooks.length > 1) {
+        var reset = confirm("Click OK to remove " + addedBooks + " books from the cart");
+    } else if (addedBooks.length == 1) {
+        var reset = confirm("Click OK to remove " + addedBooks + " book from the cart");
+    } else {
+        alert("Cart is empty.")
+    }
     if (reset) {
         counter = 0;
+        addedBooks.length = 0;
         var checkBox = document.getElementsByClassName("checkBox");
         var cartCounter = document.getElementById("counter");
         cartCounter.innerText = "(" + counter + ")";
-        for (i in checkBox) {
+        for (var i in checkBox) {
             if (checkBox[i].checked) {
                 checkBox[i].checked = false;
             }
